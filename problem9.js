423508a934bdc3899d9d5392343830a676f375b1
Divya Asthana <divyakumar12a48@gmail.com>
2025-11-24 00:23:07 +0530

    Add files via upload

---
+++ problem9.js
@@ -0,0 +1,39 @@
+// let a = 9;
+// console.log("Value of a in the global scope:", a);
+// {
+//     let a = 30;
+//     console.log("value of a in the block scope:", a);
+// }
+
+// let a = 8;
+// let b = "Striver";
+// let c = 777777777777777n;
+// let g = null;
+// let h = Symbol;
+// let j = true;
+// let p= 888888888n + 222222222n;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);  
+
+// const a = 10;
+// let b = "string" // const variable cannot be re-declared or updated
+// console.log(a);
+
+
+// let a = {
+//     "name " : "Striver",
+//     "age" : 22,
+//     "isStudent" : true
+// } 
+// a['value'] = 8898;
+// a['car model'] = "BMW";
+// console.log(a);
+
+//  let a12 = {
+//     "right " : "left",
+//     "up " : "down",
+//     "front " : "back",
+// }
+// a12 ['top'] = "bottom";
+// console.log(a12);//
