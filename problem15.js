79ed9ec0bad46de8a3850d798b430dc5e2672c0e
Divya Asthana <divyakumar12a48@gmail.com>
2025-11-29 00:35:48 +0530

    Add files via upload

---
+++ problem15.js
@@ -0,0 +1,15 @@
+// const mean = (a,b,c,d) =>{
+//     return(a+b+c+d)/2;
+
+// }
+// console.log(mean(2,4,6,8));
+
+
+// let name = "Harry";
+// console.log(name.length);
+
+let a  = "pramod";
+let b = " devi ";
+
+let sentence = `${a} IS A GOOF FREIEND OF ${b}`;// using backtick to create template string
+console.log(sentence);
