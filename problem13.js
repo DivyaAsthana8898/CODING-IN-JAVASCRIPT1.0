68bf0dcab412ce34fdf9728b7e8e10e055b59ca7
Divya Asthana <divyakumar12a48@gmail.com>
2025-11-29 00:36:28 +0530

    Add files via upload

---
+++ problem13.js
@@ -0,0 +1,10 @@
+let marks = {
+    "Harry " :69,
+    "jayant ": 34,
+    "Rohan ":88,
+    "Saksham":97
+}
+ for (let key in marks){// for in loop to iterate over keys of object
+    console.log("THE MARKS OF STUDENTS  " + key + " are "+ marks[key]);
+
+ }
