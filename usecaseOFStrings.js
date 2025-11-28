1d3a857aae130d6de3e5eb49d3a7153f2fb112f5
Divya Asthana <divyakumar12a48@gmail.com>
2025-11-29 00:34:44 +0530

    Add files via upload

---
+++ usecaseOFStrings.js
@@ -0,0 +1,14 @@
+// let name = "Harry";
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(0,3));
+// console.log(name.replace("Har" , "Per"))
+
+
+// let friend1 = "pramod"
+// let friend2 = "devi"
+// console.log(friend1.concat(" IS A GOOD FRIEND OF ",friend2));
+
+let a = "            MEENA       ";
+console.log(a.trim())
